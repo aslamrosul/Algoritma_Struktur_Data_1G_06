c81beef94b941adb0d45166c155e7d33414ec79c
aslamrosul <nvrsrndrmrx@gmail.com>
2024-02-20 08:41:32 +0700

    P2_percobaan2_06

--- P2/BukuMain06.java
+++ P2/BukuMain06.java
@@ -10,7 +10,7 @@ public class BukuMain06 {
         bk1.harga = 71000;
 
         bk1.tampilInformasi();
-        bk1.terjual(5);
+        bk1.terjual(5); 
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
     }
