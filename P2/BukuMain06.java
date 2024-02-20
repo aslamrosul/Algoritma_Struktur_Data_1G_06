d70589904afccad224f48117d8c8f50a3341e17f
aslamrosul <nvrsrndrmrx@gmail.com>
2024-02-20 08:26:10 +0700

    P2_percobaan2_06

---
+++ P2/BukuMain06.java
@@ -0,0 +1,17 @@
+package P2;
+
+public class BukuMain06 {
+    public static void main(String[] args) {
+        Buku06 bk1 = new Buku06();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
