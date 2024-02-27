7fd26f639128b545c3470216f931f03f2e78bba6
aslamrosul <nvrsrndrmrx@gmail.com>
2024-02-27 23:40:17 +0700

    P1_percobaan2_06

--- P3/ArrayObjects.java
+++ P3/ArrayObjects.java
@@ -1,21 +1,27 @@
+
 /**
  * ArrayObjects
  */
-public class ArrayObjects {
+import java.util.Scanner;
 
-    public static void main(String[] args ) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];
-        ppArray[0] = new PersegiPanjang();
-        ppArray[0].panjang = 110;
-        ppArray[0].lebar = 30;
-        ppArray[1] = new PersegiPanjang();
-        ppArray[1].panjang = 80;
-        ppArray[1].lebar = 40;
-        ppArray[2] = new PersegiPanjang();
-        ppArray[2].panjang = 100;
-        ppArray[2].lebar = 20;
-        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
-        System.out.println("Persegi Panjang ke-1, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
-        System.out.println("Persegi Panjang ke-2, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+public class ArrayObjects  {
+
+    public static void main(String[] args) {
+        PersegiPanjang [] ppArray = new PersegiPanjang [3];
+        Scanner sc06 = new Scanner(System.in);
+      
+        for (int i = 0; i < 3; i++) {
+            ppArray[i] = new PersegiPanjang ();
+            System.out.println("Persegi panjang ke-" + i);
+            System.out.print("Masukkan panjang: ");
+            ppArray[i].panjang = sc06.nextInt();
+            System.out.print("Masukkan lebar: ");
+            ppArray[i].lebar = sc06.nextInt();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Persegi panjang ke-" + i);
+            System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
+        }
     }
 }
