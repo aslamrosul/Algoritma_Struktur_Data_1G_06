cb525d646bec84901bffc39ce9df5307659204ad
aslamrosul <nvrsrndrmrx@gmail.com>
2024-02-28 00:31:33 +0700

    P3_percobaan2_pertanyaan4_06

--- P3/ArrayObjects.java
+++ P3/ArrayObjects.java
@@ -2,14 +2,16 @@
 /**
  * ArrayObjects
  */
-
 import java.util.Scanner;
 
 public class ArrayObjects {
+    public static int banyak;
 
     public static void main(String[] args) {
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];
         Scanner sc06 = new Scanner(System.in);
+        System.out.print("Masukkan banyak persegi panjang: ");
+        banyak = sc06.nextInt();
+        PersegiPanjang[] ppArray = new PersegiPanjang[banyak];
 
         for (int i = 0; i < 3; i++) {
             ppArray[i] = new PersegiPanjang();
@@ -19,6 +21,7 @@ public class ArrayObjects {
             System.out.print("Masukkan lebar: ");
             ppArray[i].lebar = sc06.nextInt();
         }
+
         for (int i = 0; i < 3; i++) {
             System.out.println("Persegi panjang ke-" + i);
             System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
