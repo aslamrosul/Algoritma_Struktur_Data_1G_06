09af5a49a827f7935a946bf960923c0d12d7ad5b
aslamrosul <nvrsrndrmrx@gmail.com>
2024-03-03 23:22:30 +0700

    P3_percobaan3_pertanyaan2_06

---
+++ P3/Segitiga.java
@@ -0,0 +1,17 @@
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga() { 
+    } 
+
+    // Konstruktor kedua dengan dua parameter
+    public Segitiga(int a, int t) {
+         alas= a ;
+      tinggi= t ;
+    }
+
+    
+
+
+}
