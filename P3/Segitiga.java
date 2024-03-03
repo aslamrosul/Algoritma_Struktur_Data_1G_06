3d30ffa8ed4414c68dbd0557aed2bc2d7eca25d8
aslamrosul <nvrsrndrmrx@gmail.com>
2024-03-03 23:25:21 +0700

    P3_percobaan3_pertanyaan5_06

--- P3/Segitiga.java
+++ P3/Segitiga.java
@@ -26,7 +26,10 @@ public class Segitiga {
        sgArray [2] = new Segitiga (15, 6);                                                                                                                                               
         sgArray [3] = new Segitiga(25, 10);  
 
-       
+        for (int i = 0; i < 3; i++) { 
+           System.out.println("Luas segitiga ke "+i +" : " +sgArray[i].hitungLuas());
+           System.out.println("Keliling segitiga ke "+i +" : "+ sgArray[i].hitungKeliling());
+       }
 }
     
 
