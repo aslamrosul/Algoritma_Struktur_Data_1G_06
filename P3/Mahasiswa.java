abb4983ef238a75f26bd5843021ff3d48e75022e
aslamrosul <nvrsrndrmrx@gmail.com>
2024-03-04 23:11:16 +0700

    P3_Latihan Praktikum_pertanyaan1_06

--- P3/Mahasiswa.java
+++ P3/Mahasiswa.java
@@ -2,22 +2,4 @@ public class Mahasiswa {
     public  String nama, jenisKelamin;
     public  double nim, ipk;
 
-    public static double cariTerbesar(Mahasiswa[] mhsArray) {
-        double max = mhsArray[0].ipk;
-        for (int i = 1; i < mhsArray.length; i++) {
-            if (mhsArray[i].ipk > max) {
-                max = mhsArray[i].ipk;
-            }
-        }
-
-        return max;
-    }
-
-    public static double cariRataRata(Mahasiswa[] mhsArray) {
-        double total = 0;
-        for (int i = 0; i < mhsArray.length; i++) {
-            total += mhsArray[i].ipk;
-        }
-        return total / mhsArray.length;
-    }
 }
--- P3/informasiMahasiswa.java
+++ P3/informasiMahasiswa.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class informasiMahasiswa {
     public static void main(String[] args) {
         Scanner sc06 = new Scanner(System.in);
@@ -7,13 +6,10 @@ public class informasiMahasiswa {
         System.out.print("Masukkan banyak mahasiswa : ");
         jmlMhs = sc06.nextInt();
         sc06.nextLine(); // Membersihkan newline dari buffer
-
         Mahasiswa[] mhsArray = new Mahasiswa[jmlMhs];
-
         for (int i = 0; i < mhsArray.length; i++) {
             mhsArray[i] = new Mahasiswa();
         }
-
         for (int i = 0; i < mhsArray.length; i++) {
             System.out.println("Masukkan data mahasiswa ke-" + (i+1));
             System.out.print("Masukkan nama : ");
@@ -27,19 +23,14 @@ public class informasiMahasiswa {
             mhsArray[i].ipk = sc06.nextDouble();
             sc06.nextLine(); // Membersihkan newline dari buffer
         }
-
         System.out.println();
         for (int i = 0; i < mhsArray.length; i++) {
-          
             System.out.println("Data Mahasiswa ke-" + (i+1));
             System.out.println("nama : " + mhsArray[i].nama);
             System.out.println("nim : " + mhsArray[i].nim);
             System.out.println("Jenis Kelamin : " + mhsArray[i].jenisKelamin);
             System.out.println("Nilai IPK : " +  mhsArray[i].ipk);
-        }
-
-        System.out.println();
-        System.out.println("Rata-Rata IPK dari Keseluruhan Mahasiswa : " + Mahasiswa.cariRataRata(mhsArray));
-        System.out.println("IPK Terbesar dari Keseluruhan Mahasiswa : " + Mahasiswa.cariTerbesar(mhsArray));
+    }
+      
     }
 }
