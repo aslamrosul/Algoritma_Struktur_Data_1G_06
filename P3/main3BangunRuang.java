56b8fefd9b7a2ee7657022e50db8f7e8d3e5e13f
aslamrosul <nvrsrndrmrx@gmail.com>
2024-03-11 06:30:12 +0700

    merapika file

--- P3/main3BangunRuang.java
+++ P3/main3BangunRuang.java
@@ -1,40 +1,27 @@
 import java.util.Scanner;
-
 /**
  * mainLatihanPraktikum
  */
 public class main3BangunRuang {
-
     public static void main(String[] args) {
 Scanner sc06 = new Scanner(System.in);
-
         System.out.print("Masukkan Jari2 Kerucut: ");
         Kerucut.jari2 = sc06.nextDouble();
         System.out.print("Masukkan Sisi Miring Kerucut: ");
         Kerucut.sisiMiring = sc06.nextDouble();
-
-
         System.out.print("Masukkan Sisi Alas Limas: ");
         LimasSegiEmpatSamaSisi.sisiAlas = sc06.nextInt();
         System.out.print("Masukkan Tinggi Limas: ");
         LimasSegiEmpatSamaSisi.tinggiLimas  = sc06.nextInt();
         Kerucut.tinggiKerucut  = LimasSegiEmpatSamaSisi.tinggiLimas;
-        ;
-
         System.out.print("Masukkan Jari2 Bola: ");
         Bola.jari2 = sc06.nextDouble();
-       
         System.out.println("Volume kerucut: "+ Kerucut.hitungVolume());
         System.out.println("Luas Permukaan Kerucut: "+ Kerucut.hitungLuasPermukaan());
-
         System.out.println("Volume Limas Segi Empat Sama Sisi: " +LimasSegiEmpatSamaSisi.hitungVolume());
         System.out.println("Luas Permukaan Limas Segi Empat Sama Sisi: "+ LimasSegiEmpatSamaSisi.hitungLuasPermukaan());
-
         System.out.println("Volume Bola: "+ Bola.hitungVolume());
         System.out.println("Luas Permukaan Bola: "+ Bola.hitungLuasPermukaan());
-
-       
-    
     }
 }
 
