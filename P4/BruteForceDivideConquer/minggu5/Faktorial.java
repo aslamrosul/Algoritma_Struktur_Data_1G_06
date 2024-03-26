394c9ad3865c6567481d11f420c126e7ba711958
aslamrosul <nvrsrndrmrx@gmail.com>
2024-03-26 09:11:26 +0700

    P4_percobaan1_pertanyaan2_06

--- P4/BruteForceDivideConquer/minggu5/Faktorial.java
+++ P4/BruteForceDivideConquer/minggu5/Faktorial.java
@@ -1,23 +1,21 @@
 package P4.BruteForceDivideConquer.minggu5;
-
-public class Faktorial{
+public class Faktorial {
     public int nilai;
-
     int faktorialBF(int n) {
-        int fakto = 1; 
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i; //perubahan 1 menjadi i
-        }
+        int fakto = 1;
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto * i; // perubahan 1 menjadi i
+            i++;
+       }
         return fakto;
     }
-
-    int faktorialDC(int n){
-if (n==1) {
-    return 1;
-}else {
-    int fakto = n * faktorialDC(n-1);
-    return fakto;
-}
-
+    int faktorialDC(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            int fakto = n * faktorialDC(n - 1);
+            return fakto;
+        }
     }
 }
