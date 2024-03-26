86008a6da8d5397058c30345b363cb9eba886c2e
aslamrosul <nvrsrndrmrx@gmail.com>
2024-03-26 12:04:01 +0700

    P4_percobaan3_pertanyaan3_06

---
+++ P4/BruteForceDivideConquer/minggu5/MainSum.java
@@ -0,0 +1,27 @@
+package P4.BruteForceDivideConquer.minggu5;
+
+import java.util.Scanner;
+
+public class MainSum {
+    public static void main(String[] args) {
+        Scanner sc06 = new Scanner(System.in);
+        System.out.println("===============================================================");
+        System.out.println("Program Menghitung Keuntungan Total (Satuan Juta, Misal 5.9)");
+        System.out.print("Masukkan jumlah bulan : ");
+        int elm = sc06.nextInt();
+        sc06.nextLine();
+        Sum sm = new Sum(elm);
+        System.out.println("===============================================================");
+    for (int i = 0; i < sm.elemen; i++) {
+      
+        System.out.print("Masukkan untung bulan ke - "+(i+1)+" = ");
+         sm.keuntungan[i]= sc06.nextDouble();
+    }
+    System.out.println("=======================================================================");
+    System.out.println("Algoritma Brute Force");
+    System.out.println("Total kentungan perusahaan selama "+ sm.elemen + " bulan adalah = "+sm.totalBF(sm.keuntungan));
+System.out.println("=========================================================================");
+System.out.println("Algoritma Divide Conquer");
+System.out.println("Total keuntungan prubahan selama "+ sm.elemen + " bulan adalah "+ sm.totalDC(sm.keuntungan, 0, sm.elemen-1));
+}
+}
---
+++ P4/BruteForceDivideConquer/minggu5/Sum.java
@@ -0,0 +1,33 @@
+package P4.BruteForceDivideConquer.minggu5;
+public class Sum {
+    int elemen;
+    double keuntungan[], total;
+
+    Sum (int elemen){
+        this.elemen = elemen;
+        this.keuntungan = new double[elemen];
+        this.total = 0;
+    }
+
+   double totalBF(double arr[]){
+    for (int i = 0; i < arr.length; i++) {
+        total = total + arr[i];
+    }
+    return total;
+   } 
+
+   double totalDC(double arr[], int l, int r){
+    // l = index awal
+    //r = index akhir
+    //mid = inde tengah
+   if (l==r) {
+    return arr[l];  
+   }else if(l < r){
+    int mid = (l + r)/2; //perbaikan penempatan tanda / dengan + terbalik
+double lsum = totalDC(arr, l, mid-1);
+double rsum = totalDC(arr, mid+1, r);
+return lsum+rsum+arr[mid];
+   }
+    return  0;
+}
+}
