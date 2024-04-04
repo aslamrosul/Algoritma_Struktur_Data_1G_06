31b39bb93389f0a3bec5b5c7fcc2aceb8e07298c
aslamrosul <nvrsrndrmrx@gmail.com>
2024-04-05 01:51:32 +0700

    P6_percobaan2_pertanyaan3_06

--- P6/PencarianBuku06.java
+++ P6/PencarianBuku06.java
@@ -61,7 +61,7 @@ if (left  <= right) {
     mid = (right + left)/ 2;
     if (cari == listBk[mid].kodeBuku) {
         return mid;
-    } else if (listBk[mid].kodeBuku > cari) {
+    } else if (listBk[mid].kodeBuku < cari) {
         return FindBinarySearch(cari, left, mid-1);
     } else {
         return FindBinarySearch(cari, mid+1, right);
