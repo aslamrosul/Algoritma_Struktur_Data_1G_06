b41652a4262ca2a191028ba796124843f5804815
aslamrosul <nvrsrndrmrx@gmail.com>
2024-04-30 23:50:03 +0700

    P8_percobaan2_06

---
+++ P8/Praktikum2/Nasabah06.java
@@ -0,0 +1,18 @@
+package P8.Praktikum2;
+
+public class Nasabah06 {
+    String norek, nama, alamat;
+int umur;
+double saldo;
+
+Nasabah06(){
+    
+}
+    Nasabah06 (String norek, String nama, String alamat, int umur, double saldo){
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }
+}
---
+++ P8/Praktikum2/QueueMain06.java
@@ -0,0 +1,56 @@
+package P8.Praktikum2;
+import java.util.Scanner;
+public class QueueMain06 {
+    public static void menu() {
+        System.out.println("Pilih menu: ");
+        System.out.println("1. Antrian baru");
+        System.out.println("2. Antrian keluar");
+        System.out.println("5. Cek Antrian terdepan");
+        System.out.println("4. Cek Semua Antrian");
+        System.out.println("---------------");
+    }
+    public static void main(String[] args) {
+        Scanner sc06 = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas queue: ");
+        int jumlah = sc06.nextInt();
+        Queue06 antri = new Queue06(jumlah);
+int pilih;
+        do {
+            menu();
+            pilih = sc06.nextInt();
+            sc06.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("No Rekening: ");
+                    String norek = sc06.nextLine();
+                    System.out.print("Nama: ");
+                    String nama = sc06.nextLine();
+                    System.out.print("Alamat: ");
+                    String alamat = sc06.nextLine();
+                    System.out.print("Umur: ");
+                    int umur = sc06.nextInt();
+                    System.out.print("Saldo: ");
+                    double saldo = sc06.nextDouble();
+                    Nasabah06 nb = new Nasabah06(norek, nama, alamat, umur, saldo);
+                    sc06.nextLine();
+                    antri.Enqueue(nb);
+                    break;
+            case 2:
+            Nasabah06 data = antri.Dequeue();
+            if (!"".equals(data.norek ) && !"".equals(data.nama ) && !"".equals(data.alamat )
+             && data.umur != 0 && data.saldo != 0 ) {
+                System.out.println("Antrian yang keluar: "+ data.norek + " "+ data.nama + " "+ data.alamat + " "
+                + data.umur + " "+ data.saldo + " ");
+                break;
+            }
+            case 3:
+            antri.peek();
+            break;
+            case 4:
+            antri.print();
+            break;
+                case 6:
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4);
+    }
+}
